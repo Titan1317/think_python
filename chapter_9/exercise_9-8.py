650d8358dc70fd7e5a339f41e0554426babe52d7
Mustafa Ali <mustafatmag1317@gmail.com>
2022-01-25 20:29:53 +0100

    Removed Scaffolding in exercise_9-8

--- chapter_9/exercise_9-8.py
+++ chapter_9/exercise_9-8.py
@@ -7,33 +7,9 @@ by Allen Downey
 
 from is_palindrome import is_palindrome
 
-def has_duplicates(word:str) -> bool:
-    i = 0
-    while i < len(word) - 1:
-        if word[i] in word[i+1:]:
-            return True
-        i += 1
-    return False
-        
-
-
-def solve_puzzle(number:str) -> bool:
-    first_half = number[:3]
-    second_half = number[3:]
-    if has_duplicates(first_half) or has_duplicates(second_half):
-        return True
-    for digit in first_half:
-        if digit in second_half:
-            start = number.index(digit) 
-            end = number.index(digit,3)
-            difference =abs(start - end)
-            if difference > 2:
-                return is_palindrome(number[start + 1: end])
-            return True
-    return False
 
 if __name__ == '__main__':
-    i = 300000
+    i = 100000
     while i < 1000000:
         number = str(i)
         number_1 = str(i + 1)
